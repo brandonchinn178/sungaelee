918c04854dce895a4281955f20f04487d3fc453b
Brandon Chinn <brandonchinn178@gmail.com>
2015-06-05 13:06:40 -0700

    Add login page/links

--- sungaelee/admin/views.py
+++ sungaelee/admin/views.py
@@ -1,4 +1,18 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
-class AdminHomeView(TemplateView):
+def admin_mixin(view_cls):
+    """
+    A function that generates a superclass for all Admin views. This superclass checks to
+    see if the user is logged in; otherwise will redirect to the login page.
+    """
+    class AdminMixin(view_cls):
+        def dispatch(self, request, *args, **kwargs):
+            if request.user.is_authenticated():
+                super(AdminMixin, self).dispatch(request, *args, **kwargs)
+            else:
+                return redirect('login')
+    return AdminMixin
+
+class AdminHomeView(admin_mixin(TemplateView)):
     template_name = 'admin/home.html'
--- sungaelee/base/views.py
+++ sungaelee/base/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import login as django_login
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('admin:home')
+    else:
+        return django_login(request, template_name='base/login.html')
 
 class HomeView(TemplateView):
     template_name = 'base/home.html'
