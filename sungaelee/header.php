b40566c69b86607c324226a4e352e61e7f3bf818
Brandon Chinn <brandonchinn178@gmail.com>
2015-07-13 18:46:04 -0700

    favicon

--- sungaelee/header.php
+++ sungaelee/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>" />
         <title><?php page_title(); ?></title>
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600">
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Cinzel+Decorative">
