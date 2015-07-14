2d4ce1b64dae5b15763c22951f0ed552f4bcf507
Brandon Chinn <brandonchinn178@gmail.com>
2015-07-13 19:57:42 -0700

    pause video on media page if close

--- sungaelee/js/page-media.js
+++ sungaelee/js/page-media.js
@@ -1,4 +1,8 @@
 jQuery(document).ready(function($) {
+    // enable javascript youtube api on iframe elements
+    $("iframe").attr("src", $("iframe").attr("src") + "&enablejsapi=1");
+
+    // show popup on click
     $(".zoom").click(function() {
         $("body").addClass("unscrollable");
         $(this).parents(".media").children(".focus-container").show();
@@ -9,7 +13,21 @@ jQuery(document).ready(function($) {
 
     // close on focus container or the close button
     $(".close, .focus-container").click(function() {
-        $(".focus-container").hide();
+        var container = $(this);
+        if (!container.hasClass("focus-container")) {
+            container = $(this).parents(".focus-container");
+        }
+
+        var iframe = container.find("iframe");
+        if (iframe.length > 0) {
+            // pause video
+            container.find("iframe").get(0).contentWindow.postMessage(JSON.stringify({
+                "event": "command",
+                "func": "pauseVideo"
+            }), "*");
+        }
+
+        container.hide();
         $("body").removeClass("unscrollable");
     });
 
