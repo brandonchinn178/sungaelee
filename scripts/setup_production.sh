4ece678800a56d3b252eb04f91ebd83d36e6afff
Brandon Chinn <brandonchinn178@gmail.com>
2015-07-15 18:37:29 -0700

    remove sudo

--- scripts/setup_production.sh
+++ scripts/setup_production.sh
@@ -3,12 +3,12 @@ echo -e "\nSetting up production server..."
 echo -e "\nDownloading the wordpress command line interface..."
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 
 echo -e "\nLinking apache directories..."
-sudo ln -fs ~/sungaelee ~/public_html/wp-content/themes/sungaelee
+ln -fs ~/sungaelee ~/public_html/wp-content/themes/sungaelee
 
 echo -e "\nRestarting apache workers..."
-sudo /etc/init.d/apache2 restart
+/etc/init.d/apache2 restart
 
 ./setup.sh
